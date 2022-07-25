000169259208d20096afc6bbc1ec35d668d0bb9d
mansinakrani <mansinakrani006@gmail.com>
2022-07-25 18:09:43 +0530

    SSG with Dynamic Parameters

--- next-prerendering-demo/pages/index.js
+++ next-prerendering-demo/pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Next JS Pre-rendering Documentation</h1>
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+      <Link href='/posts'>
+        <a>Posts</a>
+      </Link>
+    </>
+  )
 }
+
+export default Home
---
+++ next-prerendering-demo/pages/posts/[postId].js
@@ -0,0 +1,24 @@
+function Post({ post }) {
+    return (
+        <>
+            <h2>{ post.id } - { post.title }</h2>
+            <p>{ post.body }</p>
+            <hr />
+        </>
+    )
+}
+
+export default Post
+
+export async function getStaticProps(context) {
+    const { params } = context
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        props: {
+            posts: data,
+        },
+    }
+}

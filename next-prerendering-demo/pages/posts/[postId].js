cef11658fa5b67d2eaecfaed4bdc3bf344d02097
mansinakrani <mansinakrani006@gmail.com>
2022-07-25 18:17:02 +0530

    SSG with getStaticPaths

--- next-prerendering-demo/pages/posts/[postId].js
+++ next-prerendering-demo/pages/posts/[postId].js
@@ -10,6 +10,21 @@ function Post({ post }) {
 
 export default Post
 
+export async function getStaticPaths() {
+    return {
+        paths: [ {
+            params: { postId: '1' },
+        },
+        {
+            params: { postId: '2' },
+        },
+        {
+            params: { postId: '3' },
+        }],
+        fallback: false,
+    }
+}
+
 export async function getStaticProps(context) {
     const { params } = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
@@ -18,7 +33,7 @@ export async function getStaticProps(context) {
 
     return {
         props: {
-            posts: data,
+            post: data,
         },
     }
 }

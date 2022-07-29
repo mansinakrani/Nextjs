07b5248ad74cce5d28c35258c14c91f5924840e2
mansinakrani <mansinakrani006@gmail.com>
2022-07-29 18:18:04 +0530

    getSession - Securing Pages Client-side

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,5 +1,33 @@
-function Dashboard() {
-    return <h1>Dashboard page</h1>
-}
+// function Dashboard() {
+//     return <h1>Dashboard page</h1>
+// }
   
+// export default Dashboard
+
+import { useState, useEffect } from 'react'
+import { getSession, signIn } from 'next-auth/react'
+
+function Dashboard() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession()
+      console.log({ session })
+      if (!session) {
+        signIn()
+      } else {
+        setLoading(false)
+      }
+    }
+
+    securePage()
+  }, [])
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+  return <h1>Dashboard page</h1>
+}
+
 export default Dashboard
